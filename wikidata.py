f5b4baa4083603e703ec2142946d15763c35f8f0
Lorenzo Gobbo <106741440+logo94@users.noreply.github.com>
2022-10-30 11:11:47 +0100

    Update wikidata.py

--- wikidata.py
+++ wikidata.py
@@ -9,26 +9,26 @@ import tkinter as tk
 from tkinter import filedialog
 
 
-url = "https://www.wikidata.org/wiki/Wikidata:Main_Page"
+url = "https://www.wikidata.org/wiki/Wikidata:Main_Page" # URL della pagina principale di Wikidata
 
 root = tk.Tk()
 root.withdraw()
-file = filedialog.askopenfilename()
+file = filedialog.askopenfilename() # Selezione del foglio di calcolo
 
-fire_driver = GeckoDriverManager().install()
+fire_driver = GeckoDriverManager().install() # Installazione del driver
 driver = Firefox(service=FirefoxService(fire_driver))
 driver.maximize_window()
-driver.get(url)
+driver.get(url) # Connessione del driver alla pagina principale di Wikidata
 
-wb = openpyxl.load_workbook(file)
+wb = openpyxl.load_workbook(file) # Apertura del foglio di calcolo per la lettura e la scrittura
 sh = wb.active
 m_row = sh.max_row
 
 def title(cell):
-    string = sh.cell(row = i, column=2).value
+    string = sh.cell(row = i, column=2).value # Impostazione del termine presente nel foglio di calcolo come variabile
     return string
 
-def get_uri(i):
+def get_uri(i): # Lo scraper carica la pagina principale di Wikidata e segue gli elementi HTML in modo da ricercare il termine desiderato
     driver.get(url)
     txt = title(i)
     driver.find_element(By.NAME, 'search').send_keys(
@@ -40,14 +40,14 @@ def get_uri(i):
     if driver.find_element(By.XPATH, '/html/body/div[3]/div[3]/div[4]/div[4]/div[2]/ul/li[1]/table/tbody/tr/td[2]/div[1]/a/span/span[1]'):
         driver.find_element(By.XPATH, '/html/body/div[3]/div[3]/div[4]/div[4]/div[2]/ul/li[1]/table/tbody/tr/td[2]/div[1]/a/span/span[1]').click()
         sleep(1)
-        abText = driver.current_url
+        abText = driver.current_url # Se l'elemento HTML esiste, l'URL della pagina viene salvato come variabile
         abCell = sh.cell(row = i, column=1)
-        abCell.value = "<" + abText + ">"
-        wb.save(file)
+        abCell.value = "<" + abText + ">" # l'URL viene trascritto all'interno del foglio di calcolo
+        wb.save(file) # alvataggio del file
     else:
-        pass
+        pass # In caso di errori il termine viene saltato e si procede con la riga successiva
 
-for i in range(1, m_row + 1):
+for i in range(1, m_row + 1): # Per ogni riga del foglio di calcolo viene eseguita l'intera procedura
     try:
         get_uri(i)
     except:
