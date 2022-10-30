b6e75edab49eac2ce8fd70c6d47923479629a92c
Lorenzo Gobbo <106741440+logo94@users.noreply.github.com>
2022-10-30 12:19:15 +0100

    Update wikipedia.py

--- wikipedia.py
+++ wikipedia.py
@@ -8,26 +8,26 @@ import tkinter as tk
 from tkinter import filedialog
 
 
-url = "https://it.wikipedia.org/wiki/Pagina_principale"
+url = "https://it.wikipedia.org/wiki/Pagina_principale" # URL della pagina principale di Wikipedia Italia
 
 root = tk.Tk()
 root.withdraw()
-file = filedialog.askopenfilename()
+file = filedialog.askopenfilename() # Selezione del foglio di calcolo
 
-fire_driver = GeckoDriverManager().install()
+fire_driver = GeckoDriverManager().install() # Installazione driver
 driver = Firefox(service=FirefoxService(fire_driver))
 driver.maximize_window()
-driver.get(url)
+driver.get(url) # Caricamento della pagina principale di Wikipedia Italia
 
-wb = openpyxl.load_workbook(file)
+wb = openpyxl.load_workbook(file) # Apertura foglio di calcolo per la lettura e la scrittura
 sh = wb.active
 m_row = sh.max_row
 
 def title(cell):
-    string = sh.cell(row = i, column=3).value
+    string = sh.cell(row = i, column=2).value # Impostazione del termine presente nel foglio di calcolo come variabile
     return string
 
-def get_uri(i):
+def get_uri(i): # Lo scraper carica la pagina principale di Wikipedia Italia e segue gli elementi HTML in modo da ricercare il termine desiderato
     driver.get(url)
     txt = title(i)
     driver.find_element(By.NAME, 'search').send_keys(
@@ -39,12 +39,12 @@ def get_uri(i):
     driver.find_element(By.XPATH,
     '/html/body/div[4]/div[2]/nav[3]/div/ul/li[8]/a/span').click()
         
-    abText = driver.current_url
+    abText = driver.current_url # Se il termine combacia con la voce Wikipedia, viene seguito l'elemento Wikidata e copiato il relativo URL
     abCell = sh.cell(row = i, column=1)
-    abCell.value = "<" + abText + ">"
-    wb.save(file)
+    abCell.value = "<" + abText + ">" # L'URL viene trascritto all'interno del foglio di calcolo
+    wb.save(file) # Salvataggio del file
 
-for i in range(1, m_row + 1):
+for i in range(1, m_row + 1): # Per ogni riga del foglio di calcolo viene eseguita l'intera procedura
     try:
         get_uri(i)
     except:
